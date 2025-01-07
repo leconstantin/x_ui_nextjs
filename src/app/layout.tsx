983b251268ca4e1a9f614a1139f342838c47136f
leconstantin <lcon69184@gmail.com>
2025-01-07 16:02:03 +0200

    added leftbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={` antialiased`}>
         <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between">
-          <div className="px-2 xsm:px-4 xxl:px-8 bg-red-200 h-screen">
+          <div className="px-2 xsm:px-4 xxl:px-8">
             <LeftBar />
           </div>
           <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray">
--- src/components/LeftBar.tsx
+++ src/components/LeftBar.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+
 const menuList = [
   {
     id: 1,
@@ -63,31 +64,67 @@ const menuList = [
   },
 ];
 
-export default function LeftBar() {
+const LeftBar = () => {
   return (
-    <div className="h-screen sticky top-0 flex flex-col justify-between pt-2 pb-8">
-      {/* logo menu button */}
-      <div>
-        {/* logo */}
-        <Link href="/">
-          <Image src="/icons/logo.svg" alt="logo" width={24} height={24} />
+    <div className="h-screen sticky top-0 flex flex-col gap-4 justify-between pt-2 pb-8">
+      {/* LOGO MENU BUTTON */}
+      <div className="flex flex-col gap-4 text-lg items-center xxl:items-start">
+        {/* LOGO */}
+        <Link href="/" className="p-2 rounded-full hover:bg-[#181818] ">
+          <Image src="icons/logo.svg" alt="logo" width={24} height={24} />
         </Link>
-        {/* Menu list */}
-        <div className="flex flex-col">
-          {menuList.map((menu) => (
-            <Link key={menu.id} href={menu.link}>
+        {/* MENU LIST */}
+        <div className="flex flex-col gap-4">
+          {menuList.map((item) => (
+            <Link
+              href={item.link}
+              className="p-2 rounded-full hover:bg-[#181818] flex items-center gap-4"
+              key={item.id}
+            >
               <Image
-                src={`/icons/${menu.icon}`}
-                alt={menu.name}
+                src={`icons/${item.icon}`}
+                alt={item.name}
                 width={24}
                 height={24}
               />
+              <span className="hidden xxl:inline">{item.name}</span>
             </Link>
           ))}
         </div>
+        {/* BUTTON */}
+        <Link
+          href="/compose/post"
+          className="bg-white text-black rounded-full w-12 h-12 flex items-center justify-center xxl:hidden"
+        >
+          <Image src="icons/post.svg" alt="new post" width={24} height={24} />
+        </Link>
+        <Link
+          href="/compose/post"
+          className="hidden xxl:block bg-white text-black rounded-full font-bold py-2 px-20"
+        >
+          Post
+        </Link>
       </div>
-      {/* user */}
-      <div></div>
+      {/* USER */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <div className="w-10 h-10 relative rounded-full overflow-hidden">
+            <Image
+              src="/general/avatar.png"
+              alt="lama dev"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className="hidden xxl:flex flex-col">
+            <span className="font-bold">Lama Dev</span>
+            <span className="text-sm text-textGray">@lamaWebDev</span>
+          </div>
+        </div>
+        <div className="hidden xxl:block cursor-pointer font-bold">...</div>
+      </div>
     </div>
   );
-}
+};
+
+export default LeftBar;
