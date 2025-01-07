1e7239a0240bfdba8dfda70216f194c01e661493
leconstantin <lcon69184@gmail.com>
2025-01-07 16:53:28 +0200

    Added imagekit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+"use client";
+import { IKImage } from "imagekitio-next";
+
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
 
 export default function Home() {
   return (
     <div>
-      Hello
+      <IKImage
+        urlEndpoint={urlEndpoint}
+        path="general/post.jpeg"
+        width={400}
+        height={400}
+        alt="Alt text"
+      />
     </div>
   );
 }
